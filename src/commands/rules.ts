c0e397dfba26df6fb2bd5d19547097f819ff6b13
BrenerViotto <brenerviotto@gmail.com>
2023-01-20 22:14:56 -0300

    added rules and music

--- src/commands/rules.ts
+++ src/commands/rules.ts
@@ -36,3 +36,66 @@ bot.onText(/\/rules/, async (msg: any) => {
     });
   }
 });
+
+bot.onText(/\/addrules (.+)/, async (msg: any, match:any) => {
+  const rulesText: string = match[1];
+  if (msg.chat.type === "private") {
+    return;
+  }
+  const rules = await prisma.group.findFirst({
+    where: {
+      chat_id: msg.chat.id,
+    },
+  });
+
+  if(rules === null) {
+    return;
+  }
+
+  await prisma.group.update({
+    where: {
+      id: rules.id,
+    },
+    data: {
+      rules: msg.text.substring(10),
+    },
+  }).then((data) => {
+    bot.sendMessage(msg.chat.id, i18next.t("rules_updated"), {
+      parse_mode: "HTML",
+      reply_to_message_id: msg.message_id,
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+bot.onText(/\/rmrules/, async (msg: any, match:any) => {
+  if (msg.chat.type === "private") {
+    return;
+  }
+  const rules = await prisma.group.findFirst({
+    where: {
+      chat_id: msg.chat.id,
+    },
+  });
+
+  if(rules === null) {
+    return;
+  }
+
+  await prisma.group.update({
+    where: {
+      id: rules.id,
+    },
+    data: {
+      rules: null,
+    },
+  }).then((data) => {
+    bot.sendMessage(msg.chat.id, i18next.t("rules_removed"), {
+      parse_mode: "HTML",
+      reply_to_message_id: msg.message_id,
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
--- src/commands/youtube.ts
+++ src/commands/youtube.ts
@@ -1,29 +1,72 @@
 import bot from "../index";
 
-const ytdl = require("ytdl-core");
 const fs = require("fs");
+const ytdl = require("ytdl-core");
+const path = require("path");
 
-const chatId = 12345678;
-
-bot.onText(/\/youtube/, (msg: any) => {
-  //get chat id
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Aguarde, estou baixando o vídeo.");
-  const videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-
-  ytdl.getInfo(videoUrl, (err: any, info: any) => {
-    console.log(info);
-    if (err) throw err;
-    const videoTitle = `${info.title}.mp3`;
-    ytdl(videoUrl, { filter: "audioonly" })
-      .pipe(fs.createWriteStream(videoTitle))
-      .on("finish", () => {
-        bot.telegram.sendAudio(chatId, videoTitle).then(() => {
-          fs.unlink(videoTitle, (err: any) => {
-            if (err) throw err;
-            console.log(`Arquivo ${videoTitle} excluído com sucesso.`);
-          });
-        });
+bot.onText(
+  /\/youtube (.+)/,
+  (msg: Telegram.Message, match: RegExpExecArray) => {
+    let totalSize = 0;
+    let downloadedSize = 0;
+    let interval: NodeJS.Timeout;
+    let msg_to_edit: any;
+    //get chat id
+    const chatId: number = msg.chat.id;
+    bot
+      .sendMessage(chatId, "Aguarde, estou baixando o vídeo.")
+      .then((msg: any) => {
+        msg_to_edit = msg.message_id;
       });
-  });
-});
+
+    const videoUrl: string = match[1];
+
+    console.log(videoUrl);
+
+    ytdl
+      .getInfo(videoUrl)
+      .then((info: any) => {
+        const videoTitle: string = `${info.videoDetails.title}.mp3`;
+        totalSize = info.videoDetails.contentLength;
+        let stream = ytdl
+          .downloadFromInfo(info, { filter: "audioonly" })
+          .on(
+            "progress",
+            (chunkLength: number, downloaded: number, total: number) => {
+              downloadedSize = downloaded;
+              totalSize = total;
+            }
+          )
+          .pipe(fs.createWriteStream(videoTitle))
+          .on("finish", () => {
+            clearInterval(interval);
+            console.log("Finished!");
+            bot.editMessageText(`Download concluído !`, {
+              chat_id: chatId,
+              message_id: msg_to_edit,
+            });
+            const root = process.cwd();
+            let fileName = path.join(root, videoTitle);
+            console.log(fileName);
+            bot.sendAudio(chatId, fileName).then(() => {
+              fs.unlink(fileName, (err: any) => {
+                if (err) throw err;
+                console.log(`File ${fileName} deleted successfully.`);
+              });
+            });
+          });
+        interval = setInterval(() => {
+          let percent = (downloadedSize / totalSize) * 100;
+          if (percent >= 0) {
+            bot.editMessageText(
+              `${Math.round(percent)}% de download concluído`,
+              { chat_id: chatId, message_id: msg_to_edit }
+            );
+          }
+        }, 300);
+      })
+      .catch((err: any) => {
+        bot.sendMessage(chatId, "An error occurred. Please try again later.");
+      });
+  }
+);
